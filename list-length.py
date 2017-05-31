cb29f0109da0f35239bb64f10904c16f4245f578
Ben Matson <matsonb@eduroam-171-107.dyn.carleton.edu>
2017-05-30 22:58:38 -0500

    auxilary code to gather data

---
+++ list-length.py
@@ -0,0 +1,2 @@
+features = set(['hath', 'exeunt', 'nay', 'art', 'tis', 'thy', 'shall', 'thou', 'thee', 'scene', 'o', 'mine', 'pray', 'exit', 'th', 'enter', 'ay', 'lord', 'doth', 'speak'])
+print len(features)
